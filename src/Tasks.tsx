db3fed14c8acd2fe00a4fc628ccf74a71bc76595
VanoKi <kirsvano@gmail.com>
2025-05-28 20:46:11 +0300

    feat: entire Tasks

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -1,33 +1,38 @@
-import {type ChangeEvent, Fragment} from "react";
-import {changeTaskStatusAC, deleteTaskAC} from "@/model/tasks-reducer.ts";
-import List from "@mui/material/List";
-import {useAppDispatch} from "@/common/hooks/useAppDispatch.ts";
-import {TaskItem} from "@/TaskItem.tsx";
+import type {Todolist} from '@/app/App'
+import {useAppSelector} from '@/common/hooks/useAppSelector'
+import {selectTasks} from '@/model/tasks-selectors'
+import {TaskItem} from '@/TaskItem'
+import List from '@mui/material/List'
 
 type Props = {
-  
-};
-export const Tasks = (props: Props) => {
+  todolist: Todolist
+}
 
-  const dispatch = useAppDispatch()
+export const Tasks = ({todolist}: Props) => {
+  const {id, filter} = todolist
+
+  const tasks = useAppSelector(selectTasks)
+
+  const todolistTasks = tasks[id]
+  let filteredTasks = todolistTasks
+  if (filter === 'active') {
+    filteredTasks = todolistTasks.filter(task => !task.isDone)
+  }
+  if (filter === 'completed') {
+    filteredTasks = todolistTasks.filter(task => task.isDone)
+  }
 
   return (
-    <>
-      <List>
-        {filteredTasks.map(task => {
-          const deleteTaskHandler = () => {
-            dispatch(deleteTaskAC({todolistId: id, taskId: task.id}))
-          }
-
-          const changeTaskStatusHandler = (e: ChangeEvent<HTMLInputElement>) => {
-            const newStatusValue = e.currentTarget.checked
-            dispatch(changeTaskStatusAC({todolistId: id, taskId: task.id, isDone: newStatusValue}))
-          }
-          return (
-           <TaskItem/>
-          )
-        })}
-      </List>
-    </>
-  );
-};
+      <>
+        {filteredTasks.length === 0 ? (
+            <p>Тасок нет</p>
+        ) : (
+            <List>
+              {filteredTasks.map(task => (
+                  <TaskItem key={task.id} task={task} todolistId={id}/>
+              ))}
+            </List>
+        )}
+      </>
+  )
+}
